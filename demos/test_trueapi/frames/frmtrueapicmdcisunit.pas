d11e66bb5f897987ceb6b88378b361c2730c5e72
lalexs75 <alexs75@yandex.ru>
2023-08-24 13:29:14 +0300

    Фреймы для примера

---
+++ demos/test_trueapi/frames/frmtrueapicmdcisunit.pas
@@ -0,0 +1,64 @@
+unit frmTrueAPICmdCISUnit;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpjson, Forms, Controls, Graphics, Dialogs, StdCtrls, IniFiles,
+  frmTrueAPICmdAbstractUnit;
+
+type
+
+  { TfrmTrueAPICmdCISFrame }
+
+  TfrmTrueAPICmdCISFrame = class(TfrmTrueAPICmdAbstractFrame)
+    Button2: TButton;
+    Button3: TButton;
+    edtCIS: TEdit;
+    Label3: TLabel;
+    procedure Button3Click(Sender: TObject);
+  private
+
+  public
+    function FrameName:string; override;
+    procedure LoadParams(AIni:TIniFile); override;
+    procedure SaveParams(AIni:TIniFile); override;
+  end;
+
+implementation
+uses rxlogging;
+
+{$R *.lfm}
+
+{ TfrmTrueAPICmdCISFrame }
+
+procedure TfrmTrueAPICmdCISFrame.Button3Click(Sender: TObject);
+var
+  R: TJSONObject;
+begin
+  R:=FCRPTTrueAPI.ProductsInfo(edtCIS.Text);
+  if Assigned(R) then
+  begin
+    RxWriteLog(etInfo, R.FormatJSON);
+    R.Free;
+  end;
+end;
+
+function TfrmTrueAPICmdCISFrame.FrameName: string;
+begin
+
+end;
+
+procedure TfrmTrueAPICmdCISFrame.LoadParams(AIni: TIniFile);
+begin
+  inherited LoadParams(AIni);
+end;
+
+procedure TfrmTrueAPICmdCISFrame.SaveParams(AIni: TIniFile);
+begin
+  inherited SaveParams(AIni);
+end;
+
+end.
+
